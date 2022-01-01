0ad53a722d18316352acea44ee351c4d54063d0a
TrevorOdhiambo <trevodhiambo@gmail.com>
2022-01-01 10:11:29 +0300

    connection to firebase

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,6 +1,11 @@
-import React from "react"
+import React,{useStata} from "react"
 import './contact.css'
+import { db } from '../../src/firebase.js'
 const Contact = ()=>{
+
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [msg,setMsg] = useState('')
   return(
     <section className="contact">
       <h1 className="title">Contact Me</h1>
@@ -8,7 +13,12 @@ const Contact = ()=>{
 
       </div>
       <div className="contact-form">
-
+        <form id="form" >
+          <input type="text" className="name-input" placeholder="Name"/>
+          <input type="text" className="email-input" placeholder="Email"/>
+          <textarea type="text" className="message-input" placeholder="Message"/>
+          <input type="button" className="btn" value="Submit"/>
+        </form> 
       </div>
     </section>
   )

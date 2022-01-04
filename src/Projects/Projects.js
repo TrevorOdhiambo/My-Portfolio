bb3b36cf48e88994b106c7aca171040dc054fbe2
TrevorOdhiambo <trevodhiambo@gmail.com>
2022-01-04 07:35:19 +0300

    3 files modified, 1 deleted

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './projects.css'
-import Project from './Project'
 
 
-const Projects = ()=>{
+
+const Projects = (props)=>{
+
    return(
     <section className="projects">
     
       <div className="project-container">
       <h1 className="title">Projects</h1>  
-      <Project/>
+      <div className="single-project">
+      <div className="project-image"></div>
+      <div className="project-details">
+        <div className="project-name">React Sleep Tracker</div>
+        <div className="project-desc">Irregular sleeping patterns are a common problem. This web app will fulfill your needs in tracking  sleeping patterns, including duration and timings.</div>
+        <div className="project-skills"></div>
+      </div>  
+    </div>
       </div>
     </section >
 

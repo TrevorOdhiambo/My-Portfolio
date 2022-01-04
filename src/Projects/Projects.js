e4956c32e547bd7b9044e053047fcd6529a7037d
TrevorOdhiambo <trevodhiambo@gmail.com>
2022-01-04 06:58:34 +0300

    Single project modified

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './projects.css'
+import Project from './Project'
 
 
 const Projects = ()=>{
@@ -8,7 +9,7 @@ const Projects = ()=>{
     
       <div className="project-container">
       <h1 className="title">Projects</h1>  
-     
+      <Project/>
       </div>
     </section >
 
